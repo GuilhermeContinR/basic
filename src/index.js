214a5d2c79f9c08569b83fa69b53827709081cdd
Guilherme <Guilhermecontinr@gmail.com>
2021-02-24 14:05:43 -0300

    skel

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+   <div><h1>Go</h1></div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
